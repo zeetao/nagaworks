af69a857975a552bd3650d1efe364ca600c24319
zeetao <zeeqtao@gmail.com>
2024-11-06 13:54:24 +0000

    adding default format information for data

--- app/services/checkfront.rb
+++ app/services/checkfront.rb
@@ -1,52 +1,7 @@
 class Checkfront
-  checkfront_bookings_csv_path = "/home/ubuntu/environment/nagaworks/db/checkfront/checkfront_booking_until_31oct2024-awanmulan.csv"
-  checkfront_customers_csv_path = "/home/ubuntu/environment/nagaworks/db/checkfront/checkfront_customers_snapshot_06Nov2024.csv"
-  
-  # booking format = {
-  # 	"Booking"=>"Teratak Bonda",
-  # 	"Created"=>"16/11/2011 07:13 PM",
-  # 	"Start date"=>"18/11/2011",
-  # 	"End date"=>"19/11/2011",
-  # 	"Customer"=>"Su Ann",
-  # 	"Amount"=>"800.00",
-  # 	"Status"=>"Paid",
-  # 	"Booking ID"=>"CXRA-161111",
-  # 	"SKU"=>"1525657348",
-  # 	"Payment Type"=>"",
-  # 	"Date Modified"=>"16/11/2011 07:13 PM",
-  # 	"First Name"=>"Su",
-  # 	"Last Name"=>"Ann",
-  # 	"Email"=>"su-ann@palatepalette.com",
-  # 	"Customer ID"=>"TS6-531-802",
-  # 	"Source"=>"admin",
-  # 	"Partner Account"=>"",
-  # 	"Amount Paid"=>"800.00",
-  # 	"Amount Due"=>"0.00",
-  # 	"Sub-total"=>"800.00",
-  # 	"Taxes"=>"0.00",
-  # 	"Qty"=>"1",
-  # 	"Discount Code"=>"",
-  # 	"TID"=>"",
-  # 	"Provider"=>"",
-  # 	"Customer Language"=>"",
-  # 	"Check In / Out"=>"checkin",
-  # 	"Number of Adults/Children"=>"(3) admin: Palate Palette",
-  # 	"Phone (preferably with whatsapp)"=>"+60122261237",
-  # 	"Address - Street or resident name level sufficient"=>"",
-  # 	"Address"=>"",
-  # 	"Other requests"=>"",
-  # 	"Same day booking?"=>"",
-  # 	"guesthouse_name"=>"",
-  # 	"Catering Requests (10 pax and above only)"=>"",
-  # 	"customer_region"=>"14",
-  # 	"City"=>"Kuala lumpur",
-  # 	"Country"=>"Malaysia",
-  # 	"Postal / Zip"=>"",
-  # 	"Stay up to date on our newsletter."=>"",
-  # 	"Booking instructions"=>""
-  # }
   
   def self.import_customers
+    checkfront_customers_csv_path = "/home/ubuntu/environment/nagaworks/db/checkfront/checkfront_customers_snapshot_06Nov2024.csv"
     customers = csv_to_array_of_hashes(checkfront_customers_csv_path)
     
     # customer format = {
@@ -72,9 +27,74 @@ class Checkfront
     end
     
   end
-
+  
   def self.import_bookings
+    checkfront_bookings_csv_path = "/home/ubuntu/environment/nagaworks/db/checkfront/checkfront_booking_until_31oct2024-awanmulan.csv"
     bookings = csv_to_array_of_hashes(checkfront_bookings_csv_path)  
+    
+    # booking format = {
+    # 	"Booking"=>"Teratak Bonda",
+    # 	"Created"=>"16/11/2011 07:13 PM",
+    # 	"Start date"=>"18/11/2011",
+    # 	"End date"=>"19/11/2011",
+    # 	"Customer"=>"Su Ann",
+    # 	"Amount"=>"800.00",
+    # 	"Status"=>"Paid",
+    # 	"Booking ID"=>"CXRA-161111",
+    # 	"SKU"=>"1525657348",
+    # 	"Payment Type"=>"",
+    # 	"Date Modified"=>"16/11/2011 07:13 PM",
+    # 	"First Name"=>"Su",
+    # 	"Last Name"=>"Ann",
+    # 	"Email"=>"su-ann@palatepalette.com",
+    # 	"Customer ID"=>"TS6-531-802",
+    # 	"Source"=>"admin",
+    # 	"Partner Account"=>"",
+    # 	"Amount Paid"=>"800.00",
+    # 	"Amount Due"=>"0.00",
+    # 	"Sub-total"=>"800.00",
+    # 	"Taxes"=>"0.00",
+    # 	"Qty"=>"1",
+    # 	"Discount Code"=>"",
+    # 	"TID"=>"",
+    # 	"Provider"=>"",
+    # 	"Customer Language"=>"",
+    # 	"Check In / Out"=>"checkin",
+    # 	"Number of Adults/Children"=>"(3) admin: Palate Palette",
+    # 	"Phone (preferably with whatsapp)"=>"+60122261237",
+    # 	"Address - Street or resident name level sufficient"=>"",
+    # 	"Address"=>"",
+    # 	"Other requests"=>"",
+    # 	"Same day booking?"=>"",
+    # 	"guesthouse_name"=>"",
+    # 	"Catering Requests (10 pax and above only)"=>"",
+    # 	"customer_region"=>"14",
+    # 	"City"=>"Kuala lumpur",
+    # 	"Country"=>"Malaysia",
+    # 	"Postal / Zip"=>"",
+    # 	"Stay up to date on our newsletter."=>"",
+    # 	"Booking instructions"=>""
+    # }
+    
+  end
+  
+  def self.import_payments
+    checkfront_payments_csv_path = "/home/ubuntu/environment/nagaworks/db/checkfront/payments_up_to_06Nov2024.csv"
+    payments = csv_to_array_of_hashes(checkfront_payments_csv_path)
+    
+    # payments = {
+    #   "Transaction ID"=>"VCYY-311024-1",
+    #   "Date"=>"06/11/2024 04:22 PM",
+    #   "Event"=>"PAID",
+    #   "Amount"=>"550.00",
+    #   "Payment Type"=>"POS - Cash",
+    #   "User Account"=>"admin",
+    #   "Payment Customer"=>"Fumiko Konno",
+    #   "Payment Provider"=>"None",
+    #   "Provider Transaction"=>"672b273d85809",
+    #   "Booking Date"=>"31/12/2024",
+    #   "Info"=>""
+    # }
     
   end
   
