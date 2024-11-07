af01d7bda7d8a4184a5e0dc252ccdc97ea7b5718
zeetao <zeeqtao@gmail.com>
2024-11-07 14:11:40 +0000

    remove need for email confirmation to be present in the database. This is only a view requirement

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_many :bookings
   
   validates :email, confirmation: true # Requires a email_confirmation field in the form and must be the same
-  validates :name, :phone, :email, :email_confirmation, presence: true
+  validates :name, :phone, :email, presence: true
   
   
   
