4aaf6a2ff27b3fabf777a79f6a097e8ec711805d
zeetao <zeeqtao@gmail.com>
2024-11-07 14:12:15 +0000

    fixed callback method name. Also ensured nil values in amounts are removed before summing

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,14 +3,20 @@ class Payment < ApplicationRecord
   
   after_save :update_booking_aggregated_data
   
-  def update_customer_aggregated_data
+  def update_booking_aggregated_data
     booking = self.booking
     
     payments = booking.payments
     
+    total_payments = payments.pluck(:paid_amount)
+    total_payments.delete(nil)
+    
+    total_refunds = payments.pluck(:refund_amount)
+    total_refunds.delete(nil)
+    
     booking.update({
       payments_count: payments.count,
-      total_payments: (payments.pluck(:paid_amount).sum - payments.pluck(:refund_amount).sum).to_f
+      total_payments: (total_payments.sum - total_refunds.sum).to_f
     })
   end
 end
