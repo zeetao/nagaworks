e69b521eb9305b97feb50a697c420dbfb3bdf746
zeetao <zeeqtao@gmail.com>
2024-11-07 11:12:33 +0000

    automatically update customer aggregated data when bookings saved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,8 +6,12 @@ class Booking < ApplicationRecord
   after_save :update_customer_aggregated_data
   
   def update_customer_aggregated_data
+    customer = self.customer
     
-    
+    customer.update({
+      bookings_count: customer.bookings.count,
+      total_booking_value: customer.bookings.pluck(:total_price).sum.to_f
+    })
   end
   
   
