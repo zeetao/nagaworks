ff19666edbfacaae671c7144aa1bcc3d45091244
zeetao <zeeqtao@gmail.com>
2024-11-07 11:20:47 +0000

    automatically update booking aggregated data when payments saved

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,16 @@
 class Payment < ApplicationRecord
   belongs_to :booking
+  
+  after_save :update_booking_aggregated_data
+  
+  def update_customer_aggregated_data
+    booking = self.booking
+    
+    payments = booking.payments
+    
+    booking.update({
+      payments_count: payments.count,
+      total_payments: (payments.pluck(:paid_amount).sum - payments.pluck(:refund_amount).sum).to_f
+    })
+  end
 end
