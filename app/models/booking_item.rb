3cdb7858e1d3b5daa17399c768806304d7df324f
zeetao <zeeqtao@gmail.com>
2024-11-07 11:15:29 +0000

    automatically update booking aggregated data when booking items saved

--- app/models/booking_item.rb
+++ app/models/booking_item.rb
@@ -1,4 +1,14 @@
 class BookingItem < ApplicationRecord
   belongs_to :booking
   belongs_to :inventory
+  
+  after_save :update_booking_aggregated_data
+  
+  def update_customer_aggregated_data
+    booking = self.booking
+    
+    booking.update({
+      items_count: booking.booking_items.count
+    })
+  end
 end
