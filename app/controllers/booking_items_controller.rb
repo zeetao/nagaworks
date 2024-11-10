34017754e016021c34b9019fd7b5ff883479b556
zeetao <zeeqtao@gmail.com>
2024-11-10 06:58:11 +0000

    got turboframes to work for booking calendar data

--- app/controllers/booking_items_controller.rb
+++ app/controllers/booking_items_controller.rb
@@ -1,2 +1,18 @@
 class BookingItemsController < ApplicationController
+
+  def index
+    if params[:start_date].present?
+      @booking_items = BookingItem.joins(:inventory).where("inventories.category = ?", "Rooms").where("inventories.category = ?", "Rooms")
+        .where("start_date >= ?", params[:start_date].to_date)
+        .where("start_date <= ?", params[:start_date].to_date + 6.days)
+        
+      render turbo_stream: turbo_stream.replace("calendar", partial: "booking_items/calendar", locals: {booking_items: @booking_items})
+    else
+      @booking_items = BookingItem.joins(:inventory)
+        .where("inventories.category = ?", "Rooms")
+        .where("start_date >= ?", 1.month.ago)
+        .where("start_date <= ?", 1.month.ago + 6.days)
+    end
+  end
+
 end
