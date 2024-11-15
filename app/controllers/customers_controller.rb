195a2722feb74e6144a0328c276ee98d53d249d2
zeetao <zeeqtao@gmail.com>
2024-11-15 08:40:12 +0000

    commenting out the customer updating action for now

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,19 +12,15 @@ class CustomersController < ApplicationController
     end
   end
   
-  def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        # format.html { redirect_to booking_url(@customer), notice: "Booking was successfully updated." }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("notices", partial: "partials/notices", locals: {notice: "Customer was successfully updated."}) }
-        # format.json { render :show, status: :ok, location: @customer }
-      else
-        # format.html { redirect_to edit_booking_path(@customer), status: :unprocessable_entity, alert: @customer.errors }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("notices", partial: "partials/notices", locals: {alert: @customer.errors}) }
-        # format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @customer.update(customer_params)
+  #       format.turbo_stream { render turbo_stream: turbo_stream.replace("notices", partial: "partials/notices", locals: {notice: "Customer was successfully updated."}) }
+  #     else
+  #       format.turbo_stream { render turbo_stream: turbo_stream.replace("notices", partial: "partials/notices", locals: {alert: @customer.errors}) }
+  #     end
+  #   end
+  # end
   
   private
   
