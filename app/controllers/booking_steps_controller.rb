0c4c66addd5c62c9b01be2cd39d66201b3896fd1
zeetao <zeeqtao@gmail.com>
2024-11-16 15:21:10 +0000

    specifying steps in the booking process; customer, booking, booking_items, payment

--- app/controllers/booking_steps_controller.rb
+++ app/controllers/booking_steps_controller.rb
@@ -1,2 +1,10 @@
 class BookingStepsController < ApplicationController
+  include Wicked::Wizard
+  steps :customer, :booking, :booking_items, :payment
+  
+  def show
+    render_wizard
+  end
+  
+  
 end
