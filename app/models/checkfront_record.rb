69cf8dd854e1bf637829a6f270c160b9788120c7
zeetao <zeeqtao@gmail.com>
2024-12-20 09:36:28 +0000

    functions to import checkfront records

--- app/models/checkfront_record.rb
+++ app/models/checkfront_record.rb
@@ -1,4 +1,53 @@
-class CheckfrontRecord < ApplicationRecord
+require 'csv'
 
+class CheckfrontRecord < ApplicationRecord
   
+  path = "/home/ubuntu/environment/nagaworks/db/checkfront/new"
+  newbooking_filename = "#{path}/Awanmulan Bookings - New Booking.csv"
+  agodabooking_filename = "#{path}/Awanmulan Bookings - Agoda Bookings.csv"
+  airbnbbooking_filename = "#{path}/Awanmulan Bookings - Airbnb Bookings.csv"
+  checkfrontbooking_filename = "#{path}/Awanmulan Bookings - Checkfront Bookings.csv"
+  customer_filename = "#{path}/Awanmulan Bookings - Customer.csv"  
+  
+  def self.read_csv(filename)
+    data = CSV.open(filename, headers: true, header_converters: :symbol).map(&:to_h)
+  end
+  
+  def self.import(table_name, filename)
+    array_of_hashes = read_csv(filename)
+    bulk_update_create(array_of_hashes, table_name)
+  end
+  
+  def self.bulk_update_create(array_of_hashes, table_name)
+    array_of_hashes.each do |data_row_hash|
+      # Delete nil columns
+      data_row_hash.delete(nil)
+
+      checkfront_record = CheckfrontRecord.find_by(table_name: table_name, row_data: data_row_hash)
+
+      common_attributes = {
+        table_name: table_name,
+        row_data: data_row_hash
+      }
+
+      if checkfront_record.present?
+        # Check if any changes
+        if checkfront_record.row_data != data_row_hash
+          # Update the existing record
+          checkfront_record.update(common_attributes.merge({
+            status: "Changed",
+            notes: "Record has been updated. Need remigration"
+          }))
+        end
+        # do nothing if no changes
+      else
+        # Create a new record
+        CheckfrontRecord.create!(common_attributes.merge({
+          status: "Imported",
+          notes: "Newly imported. To be migrated"
+        }))
+      end
+    end
+    
+  end
 end
