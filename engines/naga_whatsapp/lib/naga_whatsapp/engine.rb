eb21130d00098d95d4a3d6ff1d526720c000d59e
Zee <zeeqtao@gmail.com>
2025-12-06 17:26:20 +0800

    parameterised the whastapp sdk gem configuration to take config from the naga_whatsapp gem

--- engines/naga_whatsapp/lib/naga_whatsapp/engine.rb
+++ engines/naga_whatsapp/lib/naga_whatsapp/engine.rb
@@ -6,8 +6,8 @@ module NagaWhatsapp
     # Put initializer code here instead of config/initializer in the main app or the engine
     initializer "naga_whatsapp.config_external_sdk" do |app|
       WhatsappSdk.configure do |config|
-        config.access_token = Rails.application.credentials.meta.access_token
-        config.api_version = "v19.0"
+        config.access_token = NagaWhatsapp.whatsapp_sdk_access_token # taken from initialiser for NagaWhatsapp in main app
+        config.api_version = NagaWhatsapp.whatsapp_sdk_api_version # taken from initialiser for NagaWhatsapp in main app
         config.logger = Logger.new(STDOUT) # optional, Faraday logger to attach
         config.logger_options = { bodies: true } # optional, they are all valid logger_options for Faraday
       end
