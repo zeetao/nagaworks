8aaf5587882a7549c7f9191986ea331b13f730a1
Zee <zeeqtao@gmail.com>
2025-12-06 17:25:27 +0800

    put a initializer set up for naga whatsapp gem

--- engines/naga_whatsapp/lib/naga_whatsapp.rb
+++ engines/naga_whatsapp/lib/naga_whatsapp.rb
@@ -2,5 +2,10 @@ require "naga_whatsapp/version"
 require "naga_whatsapp/engine"
 
 module NagaWhatsapp
-  # Your code goes here...
+  # white list parameters that can be configured in this gem
+  mattr_accessor :whatsapp_sdk_access_token, :whatsapp_sdk_api_version # Use mattr_accessor for shared config
+
+  def self.configure
+    yield self
+  end
 end
