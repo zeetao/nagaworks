abb9b54d366a0b2dcb8c007a8b5b0cba81ed5eb6
Zee <zeeqtao@gmail.com>
2025-12-11 11:38:14 +0700

    include test files in naga_whatsapp gem spec environemnt

--- engines/naga_whatsapp/spec/rails_helper.rb
+++ engines/naga_whatsapp/spec/rails_helper.rb
@@ -5,6 +5,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 require 'webmock/rspec'
+require 'fileutils'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -44,5 +45,15 @@ RSpec.configure do |config|
   
   config.before(:each) do
     allow(ActiveRecord::Base).to receive(:connection).and_return(double("connection"))
+  end
+
+  config.before(:suite) do
+    FileUtils.mkdir_p(Rails.root.join('spec', 'fixtures'))
+    FileUtils.touch(Rails.root.join('spec', 'fixtures', 'image.png'))
+    FileUtils.touch(Rails.root.join('spec', 'fixtures', 'sticker.webp'))
+  end
+
+  config.after(:suite) do
+    FileUtils.rm_rf(Rails.root.join('spec', 'fixtures'))
   end
 end
